62e040305a39ee96218ece01b3f9af0467405c7d
muoidvQI <125524337+muoidvQI@users.noreply.github.com>
2023-03-17 17:10:33 +0700

    first commit

---
+++ src/env/config.ts
@@ -0,0 +1,21 @@
+const dev = {
+    apiGateway: {
+      URL: "dev",
+    },
+  };
+  
+const prod = {
+    apiGateway: {
+      URL: "prod",
+    },
+  };
+
+const stag = {
+    apiGateway: {
+      URL: "stag",
+    },
+  };
+
+  export {dev, prod, stag};
+
+  
